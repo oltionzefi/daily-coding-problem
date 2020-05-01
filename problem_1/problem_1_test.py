459d3ae3e9dcf1787e50e9380e7ed922b2e1a89e
Oltion Zefi <oltionzefi@gmail.com>
2020-05-01 20:02:08 +0200

    Problem 1 updates with description and test

--- problem_1/problem_1_test.py
+++ problem_1/problem_1_test.py
@@ -1,17 +1,17 @@
 import unittest
-import problem_1
+from problem_1 import count_sets, count_sets_mem
 
 
-class MyTestCase(unittest.TestCase):
+class Problem1TestCase(unittest.TestCase):
     # Supposing that we have a list [3,4,5,8,9] and number 9
     set1 = [3, 4, 5, 8, 9]  # {9}, {4,5}
     number1 = 9
 
     def test_count_sets(self):
-        self.assertEqual(problem_1.count_sets(self.set1, self.number1), 2)
+        self.assertEqual(count_sets(self.set1, self.number1), 2)
 
     def test_count_sets_mem(self):
-        self.assertEqual(problem_1.count_sets_mem(self.set1, self.number1), 2)
+        self.assertEqual(count_sets_mem(self.set1, self.number1), 2)
 
 
 if __name__ == '__main__':
