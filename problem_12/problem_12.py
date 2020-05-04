b5ac21a9e615a3a081df012df26308696e1fa63a
Oltion Zefi <oltionzefi@gmail.com>
2020-05-04 23:13:21 +0200

    Problem 12 first phase

---
+++ problem_12/problem_12.py
@@ -0,0 +1,2 @@
+def unique_ways():
+    return
---
+++ problem_12/problem_12_test.py
@@ -0,0 +1,11 @@
+import unittest
+from problem_12 import unique_ways
+
+
+class Problem12TestCase(unittest.TestCase):
+    def test_unique_ways(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
