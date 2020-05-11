4fc83a2c704851af319a016fad269d42b97c7f76
Oltion Zefi <oltionzefi@gmail.com>
2020-05-11 23:06:51 +0200

    Lowest positive number

--- problem_4/problem_4.py
+++ problem_4/problem_4.py
@@ -1,14 +1,17 @@
+# Sorting array from lowest to highest will allow
+# us to do a simple check
+# 1 - If length of array will be 0, we will return 1
+# 2 - Since also for other negative value the next missing positive is 1 will initialize next_missing to 1
+# 3 - If in the loop we found something equal or lower we increase the value by 1
+
 
 def lowest_value(arr):
+    sorted(arr)
+    next_missing = 1
     if len(arr) == 0:
-        return 1
+        return next_missing
     for value in arr:
-        if value < 0:
-            continue
-        arr[value-1] = value
+        if 0 < value <= next_missing:
+            next_missing = value + 1
 
-    for value in arr:
-        if arr[value] != value + 1:
-            return value + 1
-
-    return len(arr) + 1
+    return next_missing
